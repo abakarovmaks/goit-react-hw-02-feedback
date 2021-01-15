648d722e0047d5b87f2be43d3381c11587dab415
abakarovmaks <abakarovmaks@gmail.com>
2021-01-15 10:26:29 +0200

    Добавляет компонент Section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 import React from 'react';
 import Container from './components/Container/Container';
+import Section from './components/Section/Section';
 
 export default function App() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Section />
+    </Container>
+  );
 }
